3c2a2f0da4613c5d8acce2b9ea2ccd37d5a89c2a
Arthur <artuurocha@gmail.com>
2022-10-28 17:36:51 -0300

    Add Loop Function

--- script.js
+++ script.js
@@ -19,6 +19,13 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-const playerSelection = 'Rock'
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+function game() {
+    for (let i = 0; i < 5; i++) {
+    const promptPlayer = prompt('Choose between: Rock, Paper or Scissors')
+    const playerSelection = promptPlayer
+    const computerSelection = getComputerChoice()
+    console.log(playRound(playerSelection, computerSelection))
+    }
+}
+
+game()
